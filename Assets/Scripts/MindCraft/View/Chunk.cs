148f9cf289aec1ecab0ddc623dea5945bbc2b5e7
Matěj Ošanec <matej.osanec@geewa.com>
2019-11-27 02:26:04 +0100

    basic stupid lighting based just amount of voxels above.

--- Assets/Scripts/MindCraft/View/Chunk.cs
+++ Assets/Scripts/MindCraft/View/Chunk.cs
@@ -35,15 +35,13 @@ namespace MindCraft.View
         private ChunkCoord _coords;
 
         private NativeList<float3> _vertices;
-
-        private NativeList<int> _trinagles;
-
+        private NativeList<int> _triangles;
         private NativeList<float2> _uvs;
+        private NativeList<float> _colors;
 
         private NativeArray<byte> _map;
 
         private JobHandle _jobHandle;
-        //private byte[,,] _map;
 
         [PostConstruct]
         public void PostConstruct()
@@ -73,7 +71,7 @@ namespace MindCraft.View
             set { _gameObject.SetActive(value); }
         }
 
-        
+
         #region Mesh Generation
 
         public void UpdateChunkMesh(NativeArray<byte> map)
@@ -83,15 +81,27 @@ namespace MindCraft.View
                 Debug.LogError($"<color=\"aqua\">Chunk.UpdateChunkMesh() : CHUNK ALREADY RENDERING!!!!</color>");
                 return;
             }
-            
+
             IsRendering = true;
-            
+
             _map = new NativeArray<byte>(map, Allocator.Persistent);
             _vertices = new NativeList<float3>(Allocator.Persistent);
-            _trinagles = new NativeList<int>(Allocator.Persistent);
+            _triangles = new NativeList<int>(Allocator.Persistent);
             _uvs = new NativeList<float2>(Allocator.Persistent);
-            
-            _jobHandle = CreateRenderChunkJob(_map, _vertices, _trinagles, _uvs, TextureLookup.WorldUvLookupNative, BlockDefs.TransparencyLookup);
+            _colors = new NativeList<float>(Allocator.Persistent);
+
+            var job = new RenderChunkMeshJob()
+                      {
+                          MapData = _map,
+                          Vertices = _vertices,
+                          Triangles = _triangles,
+                          Uvs = _uvs,
+                          Colors = _colors,
+                          UvLookup = TextureLookup.WorldUvLookupNative,
+                          TransparencyLookup = BlockDefs.TransparencyLookup,
+                      };
+
+            _jobHandle = job.Schedule();
 
             CoroutineManager.RunCoroutine(CheckRenderJobCoroutine());
         }
@@ -107,123 +117,174 @@ namespace MindCraft.View
         private void ProcessJobResult()
         {
             _jobHandle.Complete();
-            
+
             //process job result
             Mesh mesh = new Mesh();
             mesh.indexFormat = IndexFormat.UInt32;
             mesh.vertices = ToV3Array(_vertices);
-            mesh.triangles = _trinagles.ToArray();
+            mesh.triangles = _triangles.ToArray();
             mesh.uv = ToV2Array(_uvs);
+            mesh.colors = ToColorArray(_colors);
             mesh.RecalculateNormals();
 
             _meshFilter.mesh = mesh;
-            
+
             //dispose
             _vertices.Dispose();
-            _trinagles.Dispose();
-            _uvs.Dispose();  
+            _triangles.Dispose();
+            _uvs.Dispose();
+            _colors.Dispose();
             _map.Dispose();
-            
+
             IsRendering = false;
         }
 
         private Vector3[] ToV3Array(NativeList<float3> nl)
         {
-            var vec = new Vector3[nl.Length]; 
-            
+            var vectors = new Vector3[nl.Length];
+
             for (var i = 0; i < nl.Length; i++)
             {
-                vec[i] = nl[i];
+                vectors[i] = nl[i];
             }
 
-            return vec;
+            return vectors;
+        }
+
+        private Color[] ToColorArray(NativeList<float> nl)
+        {
+            var colors = new Color[nl.Length];
+
+            for (var i = 0; i < nl.Length; i++)
+            {
+                colors[i] = new Color(0, 0, 0, nl[i]);
+            }
+
+            return colors;
         }
 
         private Vector2[] ToV2Array(NativeList<float2> nl)
         {
-            var vec = new Vector2[nl.Length]; 
-            
+            var vectors = new Vector2[nl.Length];
+
             for (var i = 0; i < nl.Length; i++)
             {
-                vec[i] = nl[i];
+                vectors[i] = nl[i];
             }
 
-            return vec;
+            return vectors;
         }
 
-        private JobHandle CreateRenderChunkJob(NativeArray<byte> mapData, NativeList<float3> vertices, NativeList<int> triangles, NativeList<float2> uvs, NativeArray<float2> uvLookup, NativeArray<bool> transparencyLookup)
-        {
-            var job = new RenderChunkMeshJob()
-                      {
-                          MapData = mapData,
-                          Vertices = vertices,
-                          Triangles = triangles,
-                          Uvs =  uvs,
-                          UvLookup = uvLookup,
-                          TransparencyLookup = transparencyLookup,
-                      };
-            
-            return job.Schedule();    
-        }
-        
         public struct RenderChunkMeshJob : IJob
         {
             [ReadOnly] public NativeArray<byte> MapData;
             [ReadOnly] public NativeArray<float2> UvLookup;
             [ReadOnly] public NativeArray<bool> TransparencyLookup;
-            
+
             [WriteOnly] public NativeList<float3> Vertices;
             [WriteOnly] public NativeList<int> Triangles;
             [WriteOnly] public NativeList<float2> Uvs;
+            [WriteOnly] public NativeList<float> Colors;
+            
+            //[WriteOnly] public NativeArray<float> LightLevels;
 
             private int _currentVertexIndex;
 
             public void Execute()
             {
-                for(var index = 0; index < MapData.Length; index++){
-                    
-                    var voxelId = MapData[index];
-                    
-                    if(voxelId == BlockTypeByte.AIR)
-                        continue;
-                    
-                    ArrayHelper.To3D(index, out int x, out int y, out int z);
+                //LightLevels = new NativeArray<float>(MapData.Length, Allocator.TempJob);
 
-                    var position = new Vector3(x, y, z);
-                    //iterate faces
-                    for (int iF = 0; iF < FACES_PER_VOXEL; iF++)
+                //CalculateLight();
+                
+                float lightLevel = 1f;
+
+                //for(var index = 0; index < MapData.Length; index++){
+                for (var x = 0; x < VoxelLookups.CHUNK_SIZE; x++)
+                {
+                    for (var z = 0; z < VoxelLookups.CHUNK_SIZE; z++)
                     {
-                        //check neighbours
-                        var neighbour = VoxelLookups.Neighbours[iF];
-                        if (!GetTransparency(x + neighbour.x, y + neighbour.y, z + neighbour.z))
-                            continue;
-
-                        //iterate triangles
-                        for (int iV = 0; iV < TRIANGLE_INDICES_PER_FACE; iV++)
+                        for (var y = VoxelLookups.CHUNK_HEIGHT - 1; y >= 0; y--)
                         {
-                            var vertexIndex = VoxelLookups.indexToVertex[iV];
+                            //we need x, y, z at this point for light
+                            var index = ArrayHelper.To1D(x, y, z);
 
-                            // each face needs just 4 vertices & UVs
-                            if (iV < VERTICES_PER_FACE)
+                            if (y == VoxelLookups.CHUNK_HEIGHT - 1)
+                                lightLevel = 1f;
+
+                            var voxelId = MapData[index];
+
+                            if (voxelId == BlockTypeByte.AIR)
+                                continue;
+
+                            lightLevel -= (1 / 32f);
+
+                            var position = new Vector3(x, y, z);
+
+                            //iterate faces
+                            for (int iF = 0; iF < FACES_PER_VOXEL; iF++)
                             {
-                                Vertices.Add(position + VoxelLookups.Vertices[VoxelLookups.Triangles[iF, iV]]);
+                                //check neighbours
+                                var neighbour = VoxelLookups.Neighbours[iF];
+                                if (!GetTransparency(x + neighbour.x, y + neighbour.y, z + neighbour.z))
+                                    continue;
 
-                                var uvId = ArrayHelper.To1D(voxelId, iF, iV, TextureLookup.MAX_BLOCKDEF_COUNT, TextureLookup.FACES_PER_VOXEL);
-                                Uvs.Add(UvLookup[uvId]);
+                                //iterate triangles
+                                for (int iV = 0; iV < TRIANGLE_INDICES_PER_FACE; iV++)
+                                {
+                                    var vertexIndex = VoxelLookups.indexToVertex[iV];
+
+                                    // each face needs just 4 vertices & UVs
+                                    if (iV < VERTICES_PER_FACE)
+                                    {
+                                        Vertices.Add(position + VoxelLookups.Vertices[VoxelLookups.Triangles[iF, iV]]);
+
+                                        var uvId = ArrayHelper.To1D(voxelId, iF, iV, TextureLookup.MAX_BLOCKDEF_COUNT, TextureLookup.FACES_PER_VOXEL);
+                                        Uvs.Add(UvLookup[uvId]);
+
+                                        Colors.Add(lightLevel);
+                                    }
+
+                                    //we still need 6 triangle vertices tho
+                                    Triangles.Add(_currentVertexIndex + vertexIndex);
+                                }
+
+                                _currentVertexIndex += VERTICES_PER_FACE;
                             }
-
-                            //we still need 6 triangle vertices tho
-                            Triangles.Add(_currentVertexIndex + vertexIndex);
                         }
-
-                        _currentVertexIndex += VERTICES_PER_FACE;
                     }
                 }
+                
+                //LightLevels.Dispose();
             }
-            
+/*
+            private void CalculateLight()
+            {
+                float lightLevel = 1f;
+                
+                for (var x = 0; x < VoxelLookups.CHUNK_SIZE; x++)
+                {
+                    for (var z = 0; z < VoxelLookups.CHUNK_SIZE; z++)
+                    {
+                        lightLevel = 1f;
+                        
+                        for (var y = VoxelLookups.CHUNK_HEIGHT - 1; y >= 0; y--)
+                        {
+                            var index = ArrayHelper.To1D(x, y, z);
+
+                            var voxelId = MapData[index];
+                            if (voxelId != BlockTypeByte.AIR)
+                                lightLevel = TransparencyLookup[MapData[index]] ? 0.7f : 0.25f;
+
+                            //LightLevels[index] = lightLevel;
+                            LightLevels[index] = lightLevel;
+                        }
+                    }
+                }
+            }*/
+
             private bool GetTransparency(int x, int y, int z)
             {
-                //TODO: specific checks for each direction when using by face checks, dont test in rest of cases at all
+                //TODO: specific checks for each direction when using by face checks, don't test in rest of cases at all
                 if (IsVoxelInChunk(x, y, z))
                 {
                     var id = ArrayHelper.To1D(x, y, z);
